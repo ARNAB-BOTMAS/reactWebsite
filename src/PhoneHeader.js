14bfe34a717fac107cec4f8c33771cc0f3aed110
ARNAB-BOTMAS <arnabmondal203@gmail.com>
2024-01-08 19:10:29 +0530

    first commit

--- src/PhoneHeader.js
+++ src/PhoneHeader.js
@@ -1,4 +1,11 @@
-import profile from './image/profile.jpg'
+import profile from './image/profile.jpg';
+import facebook from './image/logo/Facebook.png';
+import github from './image/logo/GitHub.png';
+import instagrame from './image/logo/Instagram.png';
+import linkedin from './image/logo/LinkedIn.png';
+import telegrame from './image/logo/Telegram.png';
+import whatsapp from './image/logo/WhatsApp.png';
+
 import React, { useEffect } from 'react';
 import './phone.css';
 import Typed from 'typed.js';
@@ -38,6 +45,14 @@ const PhoneHeader = () => {
                 I'm into <span className="phone-auto-type"></span> 
             </h2>
             <button className='phone-profile-button'>About Me</button>
+            <div className='phone-profile-link'>
+                <a href="https://www.facebook.com/shagor.roy.370?mibextid=JRoKGi" target="_blank" rel="noopener noreferrer"><img src={facebook} className='phone-profile-link-img' alt="Facebook Logo"/></a>
+                <a href="https://www.instagram.com/shagor.roy.370/" target="_blank" rel="noopener noreferrer"><img src={instagrame} className='phone-profile-link-img' alt="Instagrame Logo"/></a>
+                <a href="https://github.com/ARNAB-BOTMAS" target="_blank" rel="noopener noreferrer"><img src={github} className='phone-profile-link-img' alt="GitHub Logo"/></a>
+                <a href="https://www.linkedin.com/in/arnab-mondal-4409a5299/" target="_blank" rel="noopener noreferrer"><img src={linkedin} className='phone-profile-link-img' alt="Linkedin Logo"/></a>
+                <a href="https://t.me/Arnabmon" target="_blank" rel="noopener noreferrer"><img src={telegrame} className='phone-profile-link-img' alt="Telegrame Logo"/></a>
+                <a href="https://wa.me/919339421756" target="_blank" rel="noopener noreferrer"><img src={whatsapp} className='phone-profile-link-img' alt="Whatsapp Logo"/></a>
+            </div>
         </div>
       </div>
     </header>
--- src/TabHeader.js
+++ src/TabHeader.js
@@ -1,4 +1,11 @@
-import profile from './image/profile.jpg'
+import profile from './image/profile.jpg';
+import facebook from './image/logo/Facebook.png';
+import github from './image/logo/GitHub.png';
+import instagrame from './image/logo/Instagram.png';
+import linkedin from './image/logo/LinkedIn.png';
+import telegrame from './image/logo/Telegram.png';
+import whatsapp from './image/logo/WhatsApp.png';
+
 import React, { useEffect } from 'react';
 import './tab.css';
 import Typed from 'typed.js';
@@ -38,6 +45,14 @@ const TabHeader = () => {
                 I'm into <span className="tab-auto-type"></span> 
             </h2>
             <button className='tab-profile-button'>About Me</button>
+            <div className='tab-profile-link'>
+                <a href="https://www.facebook.com/shagor.roy.370?mibextid=JRoKGi" target="_blank" rel="noopener noreferrer"><img src={facebook} className='tab-profile-link-img' alt="Facebook Logo"/></a>
+                <a href="https://www.instagram.com/shagor.roy.370/" target="_blank" rel="noopener noreferrer"><img src={instagrame} className='tab-profile-link-img' alt="Instagrame Logo"/></a>
+                <a href="https://github.com/ARNAB-BOTMAS" target="_blank" rel="noopener noreferrer"><img src={github} className='tab-profile-link-img' alt="GitHub Logo"/></a>
+                <a href="https://www.linkedin.com/in/arnab-mondal-4409a5299/" target="_blank" rel="noopener noreferrer"><img src={linkedin} className='tab-profile-link-img' alt="Linkedin Logo"/></a>
+                <a href="https://t.me/Arnabmon" target="_blank" rel="noopener noreferrer"><img src={telegrame} className='tab-profile-link-img' alt="Telegrame Logo"/></a>
+                <a href="https://wa.me/919339421756" target="_blank" rel="noopener noreferrer"><img src={whatsapp} className='tab-profile-link-img' alt="Whatsapp Logo"/></a>
+            </div>
         </div>
       </div>
     </header>
