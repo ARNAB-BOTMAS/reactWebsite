f8f742e1c61012837c6337b57be789da657a0389
ARNAB-BOTMAS <arnabmondal203@gmail.com>
2024-01-14 18:31:30 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 // import logo from './logo.svg';
 import './App.css';
-// import FrameChange from './FrameChange';
-import MyComponent from './TestDiv';
+import FrameChange from './FrameChange';
+// import MyComponent from './TestDiv';
 function App() {
   return (
     <div className="App">
-      <MyComponent />
+      <FrameChange />
     </div>
   );
 };
